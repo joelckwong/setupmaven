16b2442630a65bd336fe0c3d732bb8b73cc57867
Joel Kwong <joelckwong@yahoo.com>
2020-05-21 08:27:30 -0400

    initial commit

--- centos7.sh
+++ centos7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install java-1.8.0-openjdk-devel
-curl https://mirrors.koehn.com/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz --output apache-maven-3.6.3-bin.tar.gz 
-sudo tar -xf apache-maven-3.6.3-bin.tar.gz -C /opt
+curl https://mirrors.koehn.com/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz --output ~/apache-maven-3.6.3-bin.tar.gz 
+sudo tar -xf ~/apache-maven-3.6.3-bin.tar.gz -C /opt
 echo "export PATH=$PATH:/opt/apache-maven-3.6.3/bin" >> ~/.bash_profile
